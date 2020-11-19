de981de0b01a160053d7db98849994c7077790de
king east <1207877378@qq.com>
2020-11-19 11:41:39 +0800

    think

--- think.bat
+++ think.bat
@@ -6,4 +6,4 @@ call set "pt=%%pt:%DOCKER_PROXY_COMMAND%=%DOCKER_PROXY_COMMAND2%%%"
 call set "pt=%%pt:\=/%%"
 echo "container dir: %pt%"
 
-docker exec -it php php %pt%think %*
+docker exec -it php php %pt%/think %*
